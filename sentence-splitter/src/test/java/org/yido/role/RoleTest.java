7ee49ee886b40c345929cc0a41642387f8eb1bf9
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-09-10 17:54:13 +0900

    어절 분석기 룰 생성 테스트

--- sentence-splitter/src/test/java/org/yido/role/RoleTest.java
+++ sentence-splitter/src/test/java/org/yido/role/RoleTest.java
@@ -7,24 +7,29 @@ import org.junit.Test;
 import org.yido.dataInput.FileReader;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class RoleTest {
 
+    /**
+     *   한명의 리뷰 데이터를 조사한다.
+     *   이 때 모든 문장을 구분한 데이터를 생성하기는 어려우므로
+     *   모든 리뷰의 마지막은 문장의 끝인것을 이용하여
+     *   마지막 어절의 데이터를 수집, 분석하여 룰을 생성한다.
+     *
+     *
+     *   온점이 왔을 때 이전 어절 확인을 처리할 것
+     *  */
     @Test
     public void createRoleTest() {
         FileReader filereader = new FileReader("/data/revData.txt");
+//      role을 HashMap에 저장
+        HashMap<String, Integer> roles = new HashMap<String, Integer>();
 
 
-        /*
-        *   한명의 리뷰 데이터를 조사한다.
-        *   이 때 모든 문장을 구분한 데이터를 생성하기는 어려우므로
-        *   모든 리뷰의 마지막은 문장의 끝인것을 이용하여
-        *   마지막 어절의 데이터를 수집, 분석하여 룰을 생성한다.
-        *
-        * 
-        *   온점이 왔을 때 이전 어절 확인을 처리할 것
-        *  */
+
+
         for(String str : filereader.getSplitFile("|")) {
             System.out.println(str + " : " + str.length());
 
@@ -45,7 +50,15 @@ public class RoleTest {
                         hts.add(node);
 
                     }
-                    System.out.println(hts.get(hts.size()-1));
+
+                    String key = hts.get(hts.size()-1).morpheme().getSurface();
+
+                    if(roles.containsKey(key)){
+                        roles.replace(key, roles.get(key) + 1);
+                    } else {
+                        roles.put(key, 1);
+                    }
+                    System.out.println(hts.get(hts.size()-1).morpheme().getSurface());
 //                    System.out.println();
                 }
 //                System.out.print(eojeol.surface() + " : ");
@@ -55,6 +68,8 @@ public class RoleTest {
             }
         }
 
+        System.out.println("role size : " + roles.size());
+        System.out.println("role 다 : " + roles.get("다") );
 
 
     }
