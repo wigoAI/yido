3f757d94ae367f48a6a19e197400d7af4991e95d
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-09-17 22:24:03 +0900

    avoiding exception area

--- sentence-splitter/src/main/java/org/yido/SentenceSplitter.java
+++ sentence-splitter/src/main/java/org/yido/SentenceSplitter.java
@@ -1,9 +1,9 @@
 package org.yido;
 
-import scala.Char;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SentenceSplitter {
     private List<String> result;
@@ -14,9 +14,8 @@ public class SentenceSplitter {
     public List<String> sentenceSplit(String inputData) {
         List<Area> exceptionArea = checkExceptionArea(inputData);
 
-
-
-        for(int i = 0 ; i < inputData.length() ; i++) {
+        for(int dataIndex = 0 ; dataIndex < inputData.length() ; dataIndex++) {
+            dataIndex = avoidExceptionArea(exceptionArea, dataIndex);
 
         }
 
@@ -27,25 +26,35 @@ public class SentenceSplitter {
 
     private List<Area> checkExceptionArea(String inputData) {
         List<Area> exceptionArea = new ArrayList<>();
-        int exceptionIndex = 1;
+        Pattern bracketPattern = Pattern.compile("[\\(*\\{*\\[*][^\\)\\]\\}]*[\\)\\]\\}]");
+        Matcher matcher = bracketPattern.matcher(inputData);
 
-        while(exceptionIndex != -1) {
-//            exceptionIndex = inputData.indexOf();
+        while(matcher.find()){
+            exceptionArea.add(new Area(matcher.start(), matcher.end()));
         }
 
         return exceptionArea;
     }
 
+    private int avoidExceptionArea(List<Area> exceptionArea, int dataIndex) {
 
-    private boolean isExceptionSign(char sign) {
-        for(char exceptionSign : this.exceptionSignList) {
+        for(int listIndex = 0 ; listIndex < exceptionArea.size() ; listIndex++) {
+            Area targetArea = exceptionArea.get(listIndex);
 
-            // exceptionSign [] {} ()
-            if (exceptionSign == sign)
-                return true;
+            /** 연속된 예외구간 처리할 것
+             *  ex) (hello)(My name)
+             */
+            if(dataIndex == targetArea.getStartIndex()) {
+                dataIndex = targetArea.getEndIndex();
+                exceptionArea.remove(listIndex);
+                break;
+            }
         }
-        return false;
+
+
+        return dataIndex;
     }
+
 
     public List<String> getResult() {
         return this.result;
