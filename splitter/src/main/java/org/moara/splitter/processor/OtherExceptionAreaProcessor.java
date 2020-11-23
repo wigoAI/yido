44789255887c097f2e62e3f2b1e32eab9147a2ac
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-11-23 15:21:49 +0900

    del gradle files

---
+++ splitter/src/main/java/org/moara/splitter/processor/OtherExceptionAreaProcessor.java
@@ -0,0 +1,14 @@
+package org.moara.splitter.processor;
+
+import com.github.wjrmffldrhrl.Area;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OtherExceptionAreaProcessor {
+
+
+    public List<Area> find(String text) {
+        return new ArrayList<>();
+    }
+}
---
+++ splitter/src/test/java/org/moara/splitter/role/MetaTest.java
@@ -0,0 +1,14 @@
+package org.moara.splitter.role;
+
+import org.junit.Test;
+
+public class MetaTest {
+
+    /**
+     * Meta role read test (json)
+     */
+    @Test
+    public void testMetaRole() {
+
+    }
+}
