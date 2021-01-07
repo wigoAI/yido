00f40daa6fc9d9e848d96cfb7f0666cab041e29b
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-01-07 16:58:34 +0900

    reporter name reconizer base code

--- ner/src/main/java/org/moara/ner/ReporterRecognizer.java
+++ ner/src/main/java/org/moara/ner/ReporterRecognizer.java
@@ -1,25 +1,127 @@
+/*
+ * Copyright (C) 2021 Wigo Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.moara.ner;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+/**
+ * 기자 개체명 인식기
+ *
+ * @author wjrmffldrhrl
+ */
 public class ReporterRecognizer implements NamedEntityRecognizer {
+
+    private final static String TARGET_WORD = "기자";
+    private final static String[] SPLITTERS = {"·", "?", "/"};
+    private final static String[] exceptionWords = {"엄마", "취재", "인턴", "촬영"};
+    private final String splitterStr;
+
+
+    public ReporterRecognizer() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String splitter : SPLITTERS) {
+            stringBuilder.append("\\").append(splitter);
+        }
+
+        splitterStr = stringBuilder.toString();
+    }
+
 
     @Override
     public NamedEntity[] recognize(String corpus) {
-        corpus = corpus.replaceAll("[^가-힣]", " ");
-        String[] splitCorpus = corpus.split(" ");
 
         List<NamedEntity> reporterEntityList = new ArrayList<>();
 
-        for (int i = 1; i < splitCorpus.length; i++) {
-            if (splitCorpus[i].equals("기자")) {
-                reporterEntityList.add(new PersonEntity(splitCorpus[i - 1]));
-            }
-        }
+        reporterEntityList.addAll(recognizeBySplitSpace(corpus));
 
 
         return reporterEntityList.toArray(new NamedEntity[0]);
 
+    }
+
+    private  List<NamedEntity> recognizeBySplitSpace(String corpus) {
+        corpus = corpus.replaceAll("[^가-힣" + splitterStr + "]", " ");
+        String[] splitCorpus = corpus.split(" ");
+
+        Set<String> reporterNameSet = new HashSet<>();
+
+        // 공백으로 구분한 데이터 조회
+        for (int i = 1; i < splitCorpus.length; i++) {
+
+            if (splitCorpus[i].equals(TARGET_WORD)) {
+
+                for (String splitter : SPLITTERS) {
+
+                    if (splitCorpus[i - 1].contains(splitter)) {
+
+                        String[] reporterNames = splitCorpus[i - 1].trim().split(new StringBuilder().append("\\").append(splitter).toString());
+
+                        reporterNameSet.addAll(Arrays.asList(reporterNames));
+
+                        break;
+                    }
+                }
+
+                if (splitCorpus[i - 1].length() < 5) {
+
+                    reporterNameSet.add(splitCorpus[i - 1]);
+                }
+            }
+        }
+
+
+        // 붙어있는 기자 이름 검출
+        for (int i = 1; i < splitCorpus.length; i++) {
+
+            if (splitCorpus[i].endsWith(TARGET_WORD)) {
+                String containReporterString = splitCorpus[i].substring(0, splitCorpus[i].length() - 2);
+                for (String splitter : SPLITTERS) {
+
+                    if (containReporterString.contains(splitter)) {
+
+                        String[] reporterNames = containReporterString.trim().split(new StringBuilder().append("\\").append(splitter).toString());
+
+                        reporterNameSet.addAll(Arrays.asList(reporterNames));
+                        break;
+                    }
+                }
+
+                if (containReporterString.length() < 5) {
+                    reporterNameSet.add(containReporterString);
+                }
+            }
+        }
+
+
+        reporterNameSet = reporterNameSet.stream()
+                .map(reporterName -> reporterName.replaceAll("[" + splitterStr + "]", "")).collect(Collectors.toSet());
+
+
+
+        for (String exceptionWord : exceptionWords) {
+            reporterNameSet.removeIf(r -> r.equals(exceptionWord) || r.length() < 2);
+        }
+
+        List<NamedEntity> reporterEntityList = new ArrayList<>();
+
+        for (String reporterName : reporterNameSet) {
+            reporterEntityList.add(new PersonEntity(reporterName));
+        }
+        return reporterEntityList;
     }
 }
--- ner/src/test/java/org/moara/ner/NamedEntityRecognizerTest.java
+++ ner/src/test/java/org/moara/ner/NamedEntityRecognizerTest.java
@@ -4,6 +4,10 @@ package org.moara.ner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class NamedEntityRecognizerTest {
     @Test
     public void testReporterRecognize() {
@@ -12,12 +16,90 @@ public class NamedEntityRecognizerTest {
         NamedEntityRecognizer namedEntityRecognizer = new ReporterRecognizer();
 
         NamedEntity[] namedEntities = namedEntityRecognizer.recognize(corpus);
-
-        Assertions.assertEquals(2, namedEntities.length);
+        List<String> answerList = Arrays.asList("박종우", "김경목");
+        Assertions.assertEquals(answerList.size(), namedEntities.length);
 
         for (NamedEntity namedEntity : namedEntities) {
             System.out.println(namedEntity.getValue());
-            Assertions.assertTrue(namedEntity.getValue().equals("박종우") || namedEntity.getValue().equals("김경목"));
+            Assertions.assertTrue(answerList.contains(namedEntity.getValue()));
         }
+
+
+        corpus = "[앵커]\\n\\n탈북민 단체가 그젯밤 경기도 파주에서 기습적으로 대북전단을 살포했습니다.\\n\\n대북 전단을 두고 북한의 반발이 거세지면서, 우리 정부가  대북전단 살포를 전면 금지했는데, 경찰의 감시를 피해 기습적으로 날려보낸 겁니다.\\n\\n김경진 기자가 보도합니다.\\n\\n[리포트]\\n\\n깜깜한 밤하늘 위로, 커다란 현수막이 달린 투명한 비닐 풍선이 올라갑니다.\\n\\n22일 밤 11시에서 12시 사이,  자유북한운동연합 회원 6명이  경기도 파주에서 기습적으로  대북전단을 살포했다고 주장하며 공개한 영상입니다.\\n\\n이 단체는 대북전단 50만 장과  소책자 500권, 1달러짜리 지폐 2천 장, SD 카드 천 개를 20개의 대형 풍선에 담아 보냈다고 주장했습니다.\\n\\n그런데 이 가운데 하나로 추정되는 풍선이 어제 오전 강원도 홍천에서 발견됐습니다.\\n\\n풍선에 달린 현수막이 탈북단체가 공개한 영상의 현수막과 같습니다.\\n\\n하지만 전단은 80여 장만 달려있었습니다.\\n\\n발견된 곳은  경기도 파주에서 동쪽으로 70여 킬로미터 가량 떨어진 곳입니다.\\n\\n풍선을 날린 곳이 파주가 맞다면 밤사이 서풍을 타고 날아간 셈입니다.\\n\\n[이병유/최초 신고자 : \\\"딱 삐라(전단) 같아서 사진을 보니까, 김정은, 김여정이 사진이 보이길래 바로 신고한 거죠.\\\"]\\n\\n풍향 등을 고려했을 땐, 북쪽으로 날아간 전단은 없는 것으로 당국은 파악하고 있다고 밝혔습니다.\\n\\n또 탈북 단체가 1달러 지폐를 넣었고, 전단의 양도 50만 장이라고 주장했지만  정황상 신뢰도가 낮은 것으로 보고 있습니다.\\n\\n[홍천경찰서 관계자/음성변조 : \\\"(지폐라든가 이런 것도 같이 들어 있었나요?)그런 것은 전혀 없고요. 풍선하고 현수막 사진, 비닐 봉지 안에 대북전단이 100매 안 되게 (있었습니다.)\\\"]\\n\\n통일부는  남북 간 긴장을 고조시키고 지역 주민의 안전을 위협한다며 전단 살포 행위에 대해  엄정한 대응조치를 경고했습니다.\\n\\nKBS 뉴스 김경진입니다.\",";
+        namedEntityRecognizer = new ReporterRecognizer();
+
+        namedEntities = namedEntityRecognizer.recognize(corpus);
+        answerList = Arrays.asList("김경진");
+        Assertions.assertEquals(answerList.size(), namedEntities.length);
+
+        for (NamedEntity namedEntity : namedEntities) {
+            System.out.println(namedEntity.getValue());
+            Assertions.assertTrue(answerList.contains(namedEntity.getValue()));
+        }
+
     }
+
+    @Test
+    public void testReportersRecognize() {
+        String corpus = "담양군까지 케이블카 운영을 추진하면서 이용객 감소가 불가피해졌다.\r\n\r\n울산·창원·포항·전주=이보람·안원준·장영태·김동욱 기자 boram@segye.com\r\n\r\n";
+        NamedEntityRecognizer namedEntityRecognizer = new ReporterRecognizer();
+
+
+        List<String> answerList = Arrays.asList("이보람", "안원준", "장영태", "김동욱");
+        NamedEntity[] namedEntities = namedEntityRecognizer.recognize(corpus);
+
+        Assertions.assertEquals(answerList.size(), namedEntities.length);
+
+        for (NamedEntity namedEntity : namedEntities) {
+            System.out.println(namedEntity.getValue());
+            Assertions.assertTrue(answerList.contains(namedEntity.getValue()));
+        }
+
+
+        corpus = "직접 챙기는 상황이면 아무래도 밑에선 휴가를 다녀오기 부담스럽다\\\"며 \\\"단체장이 휴가를 미루는 바람에 덩달아 일정을 미루는 직원들도 있다\\\"고 말했다.   \\r\\n \\r\\n최모란·최은경·심석용 기자,[전국종합] moran@joongang.co.kr \\r\\n\\r\\n";
+        namedEntityRecognizer = new ReporterRecognizer();
+
+        answerList = Arrays.asList("최모란", "최은경", "심석용");
+        namedEntities = namedEntityRecognizer.recognize(corpus);
+
+        Assertions.assertEquals(answerList.size(), namedEntities.length);
+
+        for (NamedEntity namedEntity : namedEntities) {
+            System.out.println(namedEntity.getValue());
+            Assertions.assertTrue(answerList.contains(namedEntity.getValue()));
+        }
+
+
+        corpus = " 다시 효율적으로 (수사) 할 수 있을 방안을 찾겠다”고 밝혔다.  \\r\\n \\r\\n김민상?박사라?박태인 기자 kim.minsang@joongang.co.kr\\r\\n\\r\\n ■ 영장 기각 사유(박정길 영장전담";
+        namedEntityRecognizer = new ReporterRecognizer();
+
+        answerList = Arrays.asList("김민상", "박사라", "박태인");
+        namedEntities = namedEntityRecognizer.recognize(corpus);
+
+        Assertions.assertEquals(answerList.size(), namedEntities.length);
+
+        for (NamedEntity namedEntity : namedEntities) {
+            System.out.println(namedEntity.getValue());
+            Assertions.assertTrue(answerList.contains(namedEntity.getValue()));
+        }
+
+    }
+
+    @Test
+    public void testNoSpaceReporterRecognize() {
+        String corpus = "이번 행사는 코로나19 관련 방역수칙 준수하에 진행됐다.\\n\\n/이태규기자 classic@sedaily.com\\n\\n▶ 엄마 기자 취재수첩 [서지혜의 건강한 육아]\\n\\n▶ 中투자 알짜정보만 쏙쏙  [니하오 중국증시]\\n\\n▶ 네이버 채널에서 '서울경제' 구독해주세요!\\n\\n저작권자 ⓒ 서울경제, 무단 전재 및 재배포 금지";
+        NamedEntityRecognizer namedEntityRecognizer = new ReporterRecognizer();
+
+
+        List<String> answerList = Arrays.asList("이태규");
+        NamedEntity[] namedEntities = namedEntityRecognizer.recognize(corpus);
+
+
+        for (NamedEntity namedEntity : namedEntities) {
+            System.out.println("reporter : " + namedEntity.getValue());
+            Assertions.assertTrue(answerList.contains(namedEntity.getValue()));
+        }
+        Assertions.assertEquals(answerList.size(), namedEntities.length);
+    }
+
 }
