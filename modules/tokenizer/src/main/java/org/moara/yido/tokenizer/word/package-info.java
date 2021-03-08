6859d7491515ed69552b59ffcd9ff7beddb26b35
macle86 <ysys86a@gmail.com>
2021-03-08 16:31:38 +0900

    tokenizer 개발 품사태그표 정리

---
+++ modules/tokenizer/src/main/java/org/moara/yido/tokenizer/word/package-info.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * 품사 태그표는 세종테그를 기점으로표준으로 정하고
+ * mecab 에서 쓰이는 품사중 더 세분화한 부분이 있을경우 세분화 한다
+ *
+ * 자세한 내용은 README.md 에서 품사테그표 참조
+ *
+ * 단어 기반의 tokenizer
+ * @author macle
+ */
+package org.moara.yido.tokenizer.word;
